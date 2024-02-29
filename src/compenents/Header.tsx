14c0d01b31645f76bcd8df14ea987493b00adf64
Kathy_Laip <ekaterinaslapnikova@Ekaterinas-MacBook-Air.local>
2024-02-29 18:25:02 +0300

    add all adaptive and hamburger menu

--- src/compenents/Header.tsx
+++ src/compenents/Header.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import twitter from '../Assets/Twitter.svg'
+import hamMenu from '../Assets/Hamburger\ Menu.svg'
 
 export const Header = () => {
+    function toggle():void {
+        const menu = document.getElementById('hamMenuBlock') as HTMLFormElement
+        if(menu.style.display === 'flex'){
+            menu.style.display = 'none'
+        } else menu.style.display = 'flex'
+    }
+
     return (
         <div className="header pdMain">
             <div className="logo"><span className="blue-text">Chirp</span></div>
@@ -13,6 +21,14 @@ export const Header = () => {
                     <img src={twitter}/>
                     <span>Sign in with Twitter</span>
                 </button>
+            </div>
+            <div className="hamMenu">
+                <img src={hamMenu} onClick={toggle}/>
+                <div id="hamMenuBlock">
+                    <span>Home</span>
+                    <span>Pricing</span>
+                    <span>FAQ</span>
+            </div>
             </div>
         </div>
     )
